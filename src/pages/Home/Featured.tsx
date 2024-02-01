9e43d040d7ceae12d0f36a859833ba9a74dc9423
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-02-01 15:38:32 -0500

    fixed featured vstack bug

--- src/pages/Home/Featured.tsx
+++ src/pages/Home/Featured.tsx
@@ -44,89 +44,92 @@ const Featured: React.FC = () => {
     "rgba(0, 0, 0, 0.6)",
   );
 
-  return (
-    <Container
-      maxW={{ base: "100%", md: "container.lg" }}
-      mt="5"
-    >
-      <VStack align="flex-start" p="5">
-        {!loading && data && data.metadata.length > 0 && (
-          <Text
-            fontFamily="heading"
-            fontSize="4xl"
-            fontWeight="bold"
-            color="brand.green.700"
-          >
-            Featured Posts:
-          </Text>
-        )}
-        <SimpleGrid columns={{ base: 1, lg: 2 }}>
-          {!loading &&
-            data &&
-            data.metadata.map((metadata, i) => {
-              const date = new Date(metadata.publishDate);
-              const now = new Date();
-              const distance = formatDistance(date, now, {
-                addSuffix: true,
-              });
-              return (
-                <Card
-                  w={{ base: "100%", lg: "60vh" }}
-                  bg={cardBg}
-                  backdropFilter="blur(10px)"
-                  shadow="none"
-                  to={`/articles/post/${metadata.slug}`}
-                  as={ReactLink}
-                >
-                  <HStack
-                    flexDir={"column"}
-                    w="100%"
-                    spacing={5}
+  const shouldRender = !loading && data && data.metadata.length > 0;
+
+  if (shouldRender) {
+    return (
+      <Container
+        maxW={{ base: "100%", md: "container.lg" }}
+        mt="5"
+      >
+        <VStack align="flex-start" p="5">
+            <Text
+              fontFamily="heading"
+              fontSize="4xl"
+              fontWeight="bold"
+              color="brand.green.700"
+            >
+              Featured Posts:
+            </Text>
+          <SimpleGrid columns={{ base: 1, lg: 2 }}>
+            { data.metadata.map((metadata, i) => {
+                const date = new Date(metadata.publishDate);
+                const now = new Date();
+                const distance = formatDistance(date, now, {
+                  addSuffix: true,
+                });
+                return (
+                  <Card
+                    w={{ base: "100%", lg: "60vh" }}
+                    bg={cardBg}
+                    backdropFilter="blur(10px)"
+                    shadow="none"
+                    to={`/articles/post/${metadata.slug}`}
+                    as={ReactLink}
                   >
-                    <Image
-                      src={metadata.image}
-                      h="40vh"
-                      w={{ base: "100%", lg: "60vh" }}
-                      objectFit="cover"
-                      flexShrink={0}
-                      borderRadius="md"
-                    />
-                    <VStack w="100%" align="center">
-                      <Box>
-                        <Text
-                          fontFamily="heading"
-                          fontSize="4xl"
-                          fontWeight="bold"
-                        >
-                          {metadata.name}
-                        </Text>
-                        <Text
-                          color={accentColor}
-                          fontSize="sm"
-                        >
-                          {distance}
-                        </Text>
-                        <Text
-                          color={accentColor}
-                          fontSize="sm"
-                        >
-                          By: {metadata.authors.join(", ")}
-                        </Text>
-                        <Text>
-                          {metadata.description
-                            .map((r) => r.plainText)
-                            .join("")}
-                        </Text>
-                      </Box>
-                    </VStack>
-                  </HStack>
-                </Card>
-              );
-            })}
-        </SimpleGrid>
-      </VStack>
-    </Container>
-  );
+                    <HStack
+                      flexDir={"column"}
+                      w="100%"
+                      spacing={5}
+                    >
+                      <Image
+                        src={metadata.image}
+                        h="40vh"
+                        w={{ base: "100%", lg: "60vh" }}
+                        objectFit="cover"
+                        flexShrink={0}
+                        borderRadius="md"
+                      />
+                      <VStack w="100%" align="center">
+                        <Box>
+                          <Text
+                            fontFamily="heading"
+                            fontSize="4xl"
+                            fontWeight="bold"
+                          >
+                            {metadata.name}
+                          </Text>
+                          <Text
+                            color={accentColor}
+                            fontSize="sm"
+                          >
+                            {distance}
+                          </Text>
+                          <Text
+                            color={accentColor}
+                            fontSize="sm"
+                          >
+                            By: {metadata.authors.join(", ")}
+                          </Text>
+                          <Text>
+                            {metadata.description
+                              .map((r) => r.plainText)
+                              .join("")}
+                          </Text>
+                        </Box>
+                      </VStack>
+                    </HStack>
+                  </Card>
+                );
+              })}
+          </SimpleGrid>
+        </VStack>
+      </Container>
+    );
+  } else {
+    return <></>
+  }
+  
 };
 
 export default Featured;
