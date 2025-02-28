e6a5615f6580b58796ee59718cfffe0d7fad574b
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-02-28 17:56:56 -0500

    feat(client): Hero section complete with flip words and apple carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import ThemeButton from "./theme-button";
+import Hero from "./components/hero";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-primary font-display font-bold text-4xl">
-        hello world
-      </h1>
-      <ThemeButton />
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
