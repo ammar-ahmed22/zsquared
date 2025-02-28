4a4e80bba61f935a09e87166395c32bd3f7301bd
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-02-28 07:37:31 -0500

    feat(client): Add utility theme hooks + testing theme switch button + fix(client): primary foreground colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ThemeButton from "./theme-button";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-primary font-display font-bold text-4xl">
         hello world
       </h1>
+      <ThemeButton />
     </div>
   );
 }
