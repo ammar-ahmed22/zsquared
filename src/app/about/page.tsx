f6599a33cc5dd6bddf351106fbec0144a6614606
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-03-03 16:47:23 -0500

    feat[AMM-88](client): Add metadata to all pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,22 @@
+import { Metadata } from "next";
+
+const description =
+  "About the authors of Z Squared, Zaryab Ahmed and Zaid Marfatia.";
+
+export const metadata: Metadata = {
+  title: "About",
+  description,
+  openGraph: {
+    title: "About",
+    description,
+    type: "website",
+    siteName: "zsquared.ca",
+    images: [
+      `/api/og?title=About&description=${encodeURIComponent(description)}`,
+    ],
+  },
+};
+
 export default function About() {
   return <div>About page. Coming soon...</div>;
 }
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,43 @@
 import { api } from "@/lib/api";
 import Metadata from "./metadata";
 import Block from "./block";
+import { Metadata as NextMetadata } from "next";
+import { toPlainText } from "@/lib/notion/utils";
+
+export const revalidate = 60;
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  const posts = await api.posts.list({ onlyPublished: true });
+  return posts.map((post) => {
+    return {
+      slug: post.slug,
+    };
+  });
+}
+
+export async function generateMetadata({
+  params,
+}: BlogPostProps): Promise<NextMetadata> {
+  const { slug } = await params;
+  const [post] = await api.posts.list({ slug });
+  const description = toPlainText(post.description);
+  return {
+    title: post.name,
+    description,
+    openGraph: {
+      type: "article",
+      description,
+      siteName: "zsquared.ca",
+      title: post.name,
+      images: [
+        post.image ??
+          `/api/og?title=${encodeURIComponent(post.name)}&description=${encodeURIComponent(description)}`,
+      ],
+      url: "https://zsquared.ca/blog/" + post.slug,
+    },
+  };
+}
 
 export type BlogPostProps = {
   params: Promise<{ slug: string }>;
